5146fba5466d0fb6bd8f0abc6dfc9f0b2d4ce4a8
Somraj Mukherjee <iamsomraj@gmail.com>
2022-12-16 15:54:38 +0530

    added flatten method

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const arr = [1, [2, [3], 4, [5, 6, [7, 8, [9, [10]]]]]];
+
+const flatten = (arr) =>
+  arr.reduce((acc, curr) => {
+    if (Array.isArray(curr)) {
+      acc = acc.concat(flatten(curr));
+    } else {
+      acc = [...acc, curr];
+    }
+    return acc;
+  }, []);
+
+console.log(flatten(arr)); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
