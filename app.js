94870973a1c3dca4ce2b72db58bb8588ec07fd99
Somraj Mukherjee <iamsomraj@gmail.com>
2022-12-16 22:46:10 +0530

    added  throttle  click

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+const btnEl = document.getElementById('btn');
+
+const throttle = (fn, delay) => {
+  let last = 0;
+  return function (...args) {
+    let now = new Date().getTime();
+    if (now - last < delay) {
+      console.log(`Time left:  ${(now - last) / 1000} sec`);
+      return;
+    }
+    last = now;
+    return fn(...args);
+  };
+};
+
+btnEl.addEventListener(
+  'click',
+  throttle(() => {
+    console.log('Clicked');
+  }, 5000)
+);
