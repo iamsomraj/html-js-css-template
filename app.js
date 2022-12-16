d76dbf5fe9aa3bd0e42fb4f3f3901676aa3635bf
Somraj Mukherjee <iamsomraj@gmail.com>
2022-12-16 10:01:37 +0530

    added polyfill for reduce

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const arr = [1, 2, 3, 4, 5];
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    const currentElement = this[i];
+    /**
+     * Handling when initialValue is not passed
+     */
+    accumulator = accumulator ? callback(accumulator, currentElement, i, this) : currentElement;
+  }
+  return accumulator;
+};
+
+const sum = arr.myReduce((acc, curr) => acc + curr);
+console.log(sum); // 15
