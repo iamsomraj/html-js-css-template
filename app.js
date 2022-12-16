6a8c56435a9eda53a07eb57d6d0b409ac5d33689
Somraj Mukherjee <iamsomraj@gmail.com>
2022-12-16 16:39:09 +0530

    added call and bind

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+const person = {
+  firstName: 'Somraj',
+  lastName: 'Mukherjee',
+};
+
+const print = function () {
+  return this.firstName + ' ' + this.lastName;
+};
+/**
+ * @description call immediately invokes the function with the new context and arguemnts
+ * @param {*} context
+ * @param  {...any} args
+ */
+Function.prototype.myCall = function (context, ...args) {
+  context._this = this;
+  return context._this(...args);
+};
+
+/**
+ * @description bind returns a new function which can be invoked later and that function has new context and arguements
+ * @param {*} context
+ * @param {*} arg
+ */
+Function.prototype.myBind = function (context, arg) {
+  context._this = this;
+  return function () {
+    return context._this(arg);
+  };
+};
+
+const newPrint = print.myBind(person);
+console.log(newPrint());
