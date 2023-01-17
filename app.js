97211397188caca10ac25a20777cfa49d38290a9
Somraj Mukherjee <iamsomraj@gmail.com>
2023-01-17 11:50:11 +0530

    Added generators

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+// What is generators in JS?
+// Generators are functions that can be exited and later re-entered. 
+// Their context (variable bindings) will be saved across re-entrances.
+
+const number = function* () {
+    let i = 0;
+    while (true) {
+        yield i;
+        i++;
+    }
+}
+const sequence = number();
+
+
+
+console.log(sequence.next());
+console.log(sequence.next());
+console.log(sequence.next());
+console.log(sequence.next());
