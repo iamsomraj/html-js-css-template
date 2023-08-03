7e3cc9be948a027391148e83e18f3b65ba3a319a
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-03 15:18:37 +0530

    added memoize function

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+  let storedValues = {};
+  return function (...args) {
+    const jsonArgs = JSON.stringify(args);
+    if (storedValues[jsonArgs]) {
+      return storedValues[jsonArgs];
+    }
+    const value = fn(...args);
+    storedValues = {
+      ...storedValues,
+      [jsonArgs]: value,
+    };
+    return value;
+  };
+}
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
+});
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
