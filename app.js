19302476031b1cef05c6b8e2128472301f7fb990
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-14 19:31:06 +0530

    added polyfill for compose and pipe

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+function add100(x) {
+  return x + 100;
+}
+
+function double(x) {
+  return x * 2;
+}
+
+function square(x) {
+  return x * x;
+}
+
+function myCompose(...functions) {
+  return function (x) {
+    return functions.reduceRight((acc, curr) => curr(acc), x);
+  };
+}
+
+function myPipe(...functions) {
+  return function (x) {
+    return functions.reduce((acc, curr) => curr(acc), x);
+  };
+}
+
+const composedFunction = myCompose(add100, double, square);
+console.log('🚀 ~ composedFunction ');
+console.log(composedFunction(2)); // 108
+const pipedFunction = myPipe(add100, double, square);
+console.log('🚀 ~ pipedFunction ');
+console.log(pipedFunction(2)); // 41616
