6610451f6d6e5ef75eb9c892cd1467484a57df3e
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-16 18:57:54 +0530

    added polyfill for flat

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const arr = [1, [2, [3], 4, [5, 6, [7, 8, [9, [10]]]]]];
 
-const flatten = (arr) =>
-  arr.reduce((acc, curr) => {
+const flatten = (arr, depth = Infinity) => {
+  if (depth === 0) {
+    return arr;
+  }
+  return arr.reduce((acc, curr) => {
     if (Array.isArray(curr)) {
-      acc = acc.concat(flatten(curr));
+      acc = acc.concat(flatten(curr, depth - 1));
     } else {
       acc = [...acc, curr];
     }
     return acc;
   }, []);
+};
 
 console.log(flatten(arr)); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
