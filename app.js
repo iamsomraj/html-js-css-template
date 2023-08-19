774aab4e0c18c57f45ecdbd6ff35c7a8ce31136f
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-19 20:25:27 +0530

    builder pattern added

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+// Builder Pattern
+
+class CarBuilder {
+  constructor(name) {
+    this.car = { name };
+  }
+
+  addEngine(engine) {
+    this.car.engine = engine;
+    return this;
+  }
+
+  addWheels(wheels) {
+    this.car.wheels = wheels;
+    return this;
+  }
+
+  build() {
+    return this.car;
+  }
+}
+
+const car = new CarBuilder('Audi').addEngine('Audi Engine').addWheels(4).build();
+console.log(car); // {name: 'Audi', engine: 'Audi Engine', wheels: 4}
