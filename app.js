7dffccb0382b40b7273062bd0a1ca400f61f823b
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-19 20:35:35 +0530

    facade pattern added

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('Hello World');
+// Facade Pattern
+// AudioPlayer, VideoPlayer
+// MulitmediaFacade
+
+class AudioPlayer {
+  play() {
+    console.log('Audio Player ' + ' started ');
+  }
+  stop() {
+    console.log('Audio Player' + ' stopped ');
+  }
+}
+
+class VideoPlayer {
+  play() {
+    console.log('Video Player ' + ' started ');
+  }
+  stop() {
+    console.log('Video Player ' + ' stopped ');
+  }
+}
+
+class MulitmediaFacade {
+  turnOn() {
+    new AudioPlayer().play();
+    new VideoPlayer().play();
+  }
+
+  turnOff() {
+    new AudioPlayer().stop();
+    new VideoPlayer().stop();
+  }
+}
+
+const multimedia = new MulitmediaFacade();
+multimedia.turnOn();
+multimedia.turnOff();
