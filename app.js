795d3a9c4206b9d5bfc4fd3c6b910e9106827f1f
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-19 19:49:18 +0530

    added factory pattern

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Hello World');
+// Vehicle Factory
+// Factory can make both Car and Bike
+
+class Car {
+  constructor(brand) {
+    this.brand = brand;
+  }
+}
+
+class Bike {
+  constructor(brand) {
+    this.brand = brand;
+  }
+}
+
+class VehicleFactory {
+  constructor(type, brand) {
+    const vehicleMap = {
+      car: Car,
+      bike: Bike,
+    };
+    return new vehicleMap[type](brand);
+  }
+}
+
+const car = new VehicleFactory('car', 'BMW');
+console.log('car', car); // Car
+const bike = new VehicleFactory('bike', 'Honda');
+console.log('bike', bike); // Bike
