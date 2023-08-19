12605f68689111a81abb6793a7a6edcdf452591f
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-19 19:42:41 +0530

    added singleton

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+class Database {
+  constructor() {
+    if (Database.instance) {
+      return Database.instance;
+    }
+    this.data = null;
+    Database.instance = this;
+  }
+
+  setData(value) {
+    this.data = value;
+  }
+
+  getData() {
+    return this.data;
+  }
+}
+
+const db1 = new Database();
+const db2 = new Database();
+
+db1.setData('somraj');
+const data = db2.getData();
+console.log(data); // somraj
+console.log(db1 === db2); // true
