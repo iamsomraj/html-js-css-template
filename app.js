5f8fa829ce9c377f15b4a365cbd1b78ca390d966
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-19 19:57:04 +0530

    added obs pattern

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+// Observer Pattern
+// Publisher and Observer are classes
+
+class Publisher {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+
+  publish(news) {
+    this.observers.forEach((obs) => obs.update(news));
+  }
+}
+
+class Observer {
+  constructor(name) {
+    this.name = name;
+  }
+  update(news) {
+    console.log(`Hey ${this.name}, News Update - ${news}`);
+  }
+}
+
+const timesOfIndia = new Publisher();
+
+const somraj = new Observer('somraj');
+const souradip = new Observer('souradip');
+
+timesOfIndia.subscribe(somraj);
+timesOfIndia.subscribe(souradip);
+
+timesOfIndia.publish('Headine for today - ' + new Date().toDateString());
