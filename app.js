faf6b8cf5702432ef7131af50a7a17f2d563dace
Somraj Mukherjee <somraj.mukherjee@yocket.in>
2023-08-19 20:02:11 +0530

    deco pattern

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+// Decorator Pattern
+// Coffee
+// MilkDecorator
+
+class Coffee {
+  cost() {
+    return 5;
+  }
+}
+
+class MilkDecorator {
+  constructor(coffee) {
+    this.coffee = coffee;
+  }
+
+  cost() {
+    return this.coffee.cost() + 2.5;
+  }
+}
+
+const normalCoffee = new Coffee();
+const normalCoffeeCost = normalCoffee.cost();
+console.log(normalCoffeeCost); // 5
+const coffeeWithMilk = new MilkDecorator(normalCoffee);
+const coffeeWithMilkCost = coffeeWithMilk.cost();
+console.log(coffeeWithMilkCost); // 7.5
